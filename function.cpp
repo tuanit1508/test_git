1a4f6e0ffbc7b0705d2115b8ef6834dfeeedb33f
tuanit1508 <tuanpade170085@fpt.edu.vn>
2024-05-24 08:31:17 +0700

    test1

---
+++ function.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//int main() {
+//	int i, bp;
+//	for(i = 1; i <= 10; i++){
+//		bp = i * i;
+//		printf("binh phuong cua %d la: %d \n", i, bp);
+//	}
+//}
+//int tinhBinhPhuong(int x) {
+//	int ketQua;
+//	ketQua = x*x;
+//	return ketQua;
+//}
+//
+//int main() {
+//	int i;
+//	for(i = 1; i <= 10; i++) {
+//		printf("Binh phuong cua %d la: %d\n", i, tinhBinhPhuong(i));
+//	}
+//	return 0;
+//}
+
+//int find_sum(int a,int b){
+//    int sum = a + b;
+//    return sum;
+//}
+//int main(){
+//	int a,b;
+//	scanf("%d%d", &a,&b);
+//	printf("Tong cua %d va %d la: %d",a,b,find_sum(a,b));
+//	return 0;
+//}
+//Tim uoc cua so n
+void timUoc(int n){
+	int i;
+	for(i=1; i<=n; i++);
+		if(n%i==0);
+		
+	
+	
+}
+int main(){
+	int m=10;
+	printf("Cac uoc cua n la: ", timUoc(m));
+}
